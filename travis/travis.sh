3fd483f9d96beb3d2f545cecc318b7c2316da27c
Sven Seeberg <sseebergelverfeldt@suse.com>
2017-11-08 15:56:45 +0100

    Fixed variable declaration and semicolons

--- travis/travis.sh
+++ travis/travis.sh
@@ -14,14 +14,14 @@ YELLOW='\033[33;1m'
 BOLD='\033[1m'
 NC='\033[0m' # No Color
 
-mkdir -p /root/.config/daps/;
-echo DOCBOOK5_RNG_URI="https://github.com/openSUSE/geekodoc/raw/master/geekodoc/rng/geekodoc5-flat.rnc" > /root/.config/daps/dapsrc; 
-DCLIST = $(ls DC-*-all);
+mkdir -p /root/.config/daps/
+echo DOCBOOK5_RNG_URI="https://github.com/openSUSE/geekodoc/raw/master/geekodoc/rng/geekodoc5-flat.rnc" > /root/.config/daps/dapsrc 
+DCLIST=$(ls DC-*-all)
 if [ -z "$DCLIST" ] ; then
-    DCLIST = $(ls DC-*);
+    DCLIST=$(ls DC-*)
 fi
 for DCFILE in $DCLIST; do 
-    echo -e "\n${YELLOW}${BOLD}Validating $DCFILE ... ${NC}\n"; 
-    daps -vv -d $DCFILE validate || exit 1; 
-    wait;
+    echo -e "\n${YELLOW}${BOLD}Validating $DCFILE ... ${NC}\n" 
+    daps -vv -d $DCFILE validate || exit 1 
+    wait
 done
