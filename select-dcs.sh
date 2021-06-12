9bfafe804bb96ca02a940293616556f8d8e81319
Stefan Knorr <sknorr@suse.de>
2021-06-12 14:21:00 +0200

    Add per-runner allocation of DCs, do DCHASH run over the build-list
    While it would look nicer, GHA doesn't accept
    { "1": ["DC-a", "DC-b"], "2": ... } as a matrix easily anyway, and
    it also doesn't make bashing/dapsing the outcome any easier.
    
    So we just use a flat list [ "DC-a DC-b", "DC-c DC-d", ...  ] that can
    plug right into GHA's matrix strategy thing.

--- select-dcs.sh
+++ select-dcs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2143
 # Select DC files for later building/validation with DAPS
 
 RED='\e[31m'
@@ -44,8 +45,8 @@ get_dc_value() {
   grep -oP '^\s*'"$dc_attribute"'\s*=\s*.*' "$dc_file" | head -1 | sed -r -e 's/^\s*'"$dc_attribute"'\s*=\s*//' -e 's/"*//g' -e "s/'*//g" -e 's/(^\s*|\s*$)//g'
 }
 
-
-echo "Running "$(basename "$0")", "$(sha1sum "$0" | cut -b1-8)" [ verify locally with: sha1sum "$(basename "$0")" | cut -b1-8 ]."
+# shellcheck disable=SC2027,SC2046
+log + "Running "$(basename "$0")", "$(sha1sum "$0" | cut -b1-8)" [ verify locally with: sha1sum "$(basename "$0")" | cut -b1-8 ]."
 
 gha_fold "Environment variables"
   for var in GITHUB_JOB GITHUB_EVENT_NAME \
@@ -57,11 +58,26 @@ gha_fold "Environment variables"
 gha_fold --
 
 usecase='soundness'
+mergeruns='true'
+# GHA appears to allow for 4 simultaneous runners. 8 is a multiple of 4. In
+# many repos, with this setting, you will still get individual runners for
+# each DC file which at lowish numbers of runners is good for both speed and
+# aiding comprehension of results. But for repos with lots of DC files (e.g.
+# SBP's 70+ DC files) we can capitalize☭ on the avoidance of extra container
+# image downloads/inits which tends to take 1 minute each time currently.
+# Maybe upping to a maximum of twelve runners would be an option too?
+mergerun_threshold=8
+
+allow_build='true'
 
 while [[ $1 ]]; do
   case $1 in
     use-case=*)
       usecase=$(echo "$1" | cut -f2- -d'=')
+      shift
+      ;;
+    merge-runs=*)
+      [[ $(echo "$1" | cut -f2- -d'=') = 'false' ]] && mergeruns=''
       shift
       ;;
     --)
@@ -82,6 +98,7 @@ if [[ "$usecase" = 'soundness' ]]; then
   log "Checking all DC files for basic soundness"
 
   # Check /all/ DC files in repo root for basic soundness
+  # shellcheck disable=SC2125
   check_dcs=DC-*
 
   unsounddc=
@@ -134,7 +151,6 @@ elif [[ "$usecase" = 'list-build' ]]; then
 
   dir_config_repo="$HOME/build-config-repo"
   mkdir -p "$dir_config_repo"
-  cfg_git="git -C $dir_config_repo"
   # We noticed that curl'ing the config file was too slow sometimes due to
   # GitHub's caching. This is a (unfortunately slightly expensive) workaround
   # for the issue.
@@ -149,8 +165,9 @@ elif [[ "$usecase" = 'list-build' ]]; then
   xmllint --noout --noent --dtdvalid "$configdtd" "$configxml"
   code=$?
 
-  # This may be caused by intermittent networking issues, it's probably best
-  # not to fail on this (..?) or maybe it is better, FIXME: think again.
+  # This may be caused by intermittent networking issues, it would probably
+  # be best not to fail too early because of this (FIXME). However, for
+  # simplicity, we just fail here.
   [[ $code -eq 0 ]] || { fail "Cannot determine whether to build, configuration file $branchconfig is unavailable or invalid. Will not build.\n(Check the configuration at $branchconfig_repo.)\n"; }
 
   # GitHub gives us "org/repo", we really only care about the repo
@@ -174,11 +191,24 @@ elif [[ "$usecase" = 'list-build' ]]; then
    [[ $(echo -e "$relevantbranches" | grep "^$ci_branch_abbrev\$") ]]; then
     for cat in $relevantcats; do
       for branchname in "$ci_branch" "$ci_branch_abbrev"; do
-        dc_list+=$(xml sel -t -v '//doc[@cat="'"$cat"'"][@branches[contains(concat(" ",.," "), " '"$branchname"' ")]]/@doc' "$configxml")'\n'
+        dc_list_prelim+=$(xml sel -t -v '//doc[@cat="'"$cat"'"][@branches[contains(concat(" ",.," "), " '"$branchname"' ")]]/@doc' "$configxml")'\n'
       done
     done
-    dc_list=$(echo -e "$dc_list" | tr ' ' '\n' | sed -r -e 's/^(.)/DC-\1/' -e 's/^DC-DC-/DC-/' | sort -u | sed -n '/^$/ !p' | tr '\n' ' ')
-    [[ -z "$dc_list" ]] && log "No DC files enabled for building. $branchconfig is probably invalid.\n(Check the configuration at $branchconfig_repo.)\n"
+    dc_list_prelim=$(echo -e "$dc_list_prelim" | tr ' ' '\n' | sed -r -e 's/^(.)/DC-\1/' -e 's/^DC-DC-/DC-/' | sort -u | sed -n '/^$/ !p')
+    if [[ -z "$dc_list_prelim" ]]; then
+      log - "No DC files enabled for building. $branchconfig is probably invalid.\n(Check the configuration at $branchconfig_repo.)\n"
+    else
+
+      for dc in $dc_list_prelim; do
+        [[ ! -f "$dc" ]] && { log - "$dc is configured to be built but does not exist"; allow_build='false'; continue; }
+        hash=$(/docserv-dchash "$dc" "FAKE_ROOT_ID")
+        dc_list+="$hash $dc\n"
+      done
+
+      dc_list=$(echo -e "$dc_list" | sort -u | cut -d' ' -f2)
+
+    fi
+
   else
     log "The branch \"$ci_branch\" of \"$repo\" does not appear to be configured to build.\n(Check the configuration at $branchconfig_repo.)\n"
   fi
@@ -187,8 +217,55 @@ else
   fail "Use case \"$usecase\" is unknown."
 fi
 
-log + "Came up with the following list of DC files:\n$dc_list"
 
-dc_list_json=$(echo "$dc_list" | tr ' ' '\n' | sed -n '/^$/ !p' | jq -R -s -c 'split("\n")[:-1]')
+# Create a JSON of jobs, optimize for 8 runners (with 8 = $mergerun_threshold)
+
+# Do not sort $dc_list_formatted again! Builds are faster when we can reuse
+# existing profiling results, which is why we use a non-alphabetic sort for
+# the build list.
+dc_list_formatted=$(echo "$dc_list" | tr ' ' '\n' | sed -n '/^$/ !p')
+dc_list_length=$(echo -e "$dc_list_formatted" | wc -l)
+dc_per_runner=1
+early_runner=0
+runners=$dc_list_length
+if [[ "$mergeruns" = 'true' && "$dc_list_length" -gt "$mergerun_threshold" ]]; then
+  log "Merging multiple runs because there are more than $mergerun_threshold and merging runs is enabled ('merge-runs=true')."
+  dc_per_runner=$(( dc_list_length / mergerun_threshold ))
+  early_runner=$(( dc_list_length % mergerun_threshold ))
+  runners=$mergerun_threshold
+elif [[ ! "$mergeruns" = 'true' ]]; then
+  log "Not merging runs because merging runs is disabled ('merge-runs=false')."
+else
+  log "Not merging runs because there are fewer than or exactly $mergerun_threshold runs."
+fi
+dc_list_json+="["
+
+if [[ -n "$dc_list_formatted" ]]; then
+  d=1
+  for r in $(seq 1 $runners); do
+
+    dc_per_thisrunner="$dc_per_runner"
+    [[ $r -le $early_runner ]] && dc_per_thisrunner=$(( dc_per_thisrunner + 1 ))
+
+    dc_list_json+='"'
+    for i in $(seq 1 $dc_per_thisrunner); do
+      dc_list_json+=$(echo -e "$dc_list_formatted" | sed -n "$d p" )
+      [[ "$i" -lt "$dc_per_thisrunner" ]] && dc_list_json+=' '
+
+      d=$(( d + 1 ))
+    done
+    dc_list_json+='"'
+
+    [[ "$r" -lt "$runners" ]] && dc_list_json+=','
+  done
+fi
+
+dc_list_json+="]"
+
+
+log + "Generated this list of runners associated with the following DC files:\n"
+echo "$dc_list_json" | jq
+[[ "$?" -gt 0 ]] && fail "Generated JSON was invalid:\n$dc_list_json"
 
 echo "::set-output name=dc-list::$dc_list_json"
+echo "::set-output name=allow-build::$allow_build"
